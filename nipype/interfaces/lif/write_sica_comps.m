6742d6cb9c29b8ce55de150544192badd705e604
bpinsard <bpinsard@imed.jussieu.fr>
2012-01-26 21:43:08 +0100

    ica bugs fixes

--- nipype/interfaces/lif/write_sica_comps.m
+++ nipype/interfaces/lif/write_sica_comps.m
@@ -1,15 +1,19 @@
-function write_sica_comps(sica, sica_comp_filename_fmt)
-sica.header.dt=[16 0];
-for i=1:sica.nbcomp
-	d=sica.header;
-	d.fname = ['./' sprintf(sica_comp_filename_fmt, i)];
+function write_sica_comps(sica, sica_fname)
 
-	if length(size(sica.S))<3
-		[vol] = st_1Dto3D(sica.S(:,i),sica.mask);
-	else
-		vol = squeeze(sica.S(:,:,:,i));
-	end
-	[vol_c] = st_correct_vol(vol,sica.mask);
-	st_write_analyze(vol_c,d,d.fname);
-	out.files{i}=d.fname;
+ni = nifti;
+ni.dat = file_array(sica_fname, ...
+										[sica.header.dim sica.nbcomp], ...
+										[16 0], ...
+										0,1,0);
+ni.mat = sica.header.mat;
+ni.mat0= sica.header.mat;
+ni.descrip = 'ICA components';
+create(ni);
+data=zeros([sica.header.dim sica.nbcomp]);
+for i=1:sica.nbcomp
+	data(:,:,:,i) = st_1Dto3D(sica.S(:,i),sica.mask);
+end
+%data = st_correct_vol(data,sica.mask);
+for i=1:sica.nbcomp;
+	ni.dat(:,:,:,i) = data(:,:,:,i);
 end
