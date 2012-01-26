058429c5545a13fff219b00d76abd215343d4207
bpinsard <bpinsard@imed.jussieu.fr>
2012-01-26 21:45:11 +0100

    ica bugs fixes

--- nipype/interfaces/lif/write_sica_comps.m
+++ nipype/interfaces/lif/write_sica_comps.m
@@ -1,4 +1,12 @@
+<<<<<<< HEAD
+function write_sica_comps(sica, sica_comp_filename_fmt)
+sica.header.dt=[16 0];
+for i=1:sica.nbcomp
+	d=sica.header;
+	d.fname = sprintf(sica_comp_filename_fmt, i);
+=======
 function write_sica_comps(sica, sica_fname)
+>>>>>>> lif_fmri
 
 ni = nifti;
 ni.dat = file_array(sica_fname, ...
