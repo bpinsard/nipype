76c592e286f5c1483fc106e2c4abbb50caf3d5a6
bpinsard <bpinsard@imed.jussieu.fr>
2012-04-26 11:06:02 +0200

    3dBlurInMask in init

--- nipype/interfaces/afni/__init__.py
+++ nipype/interfaces/afni/__init__.py
@@ -11,4 +11,4 @@ from .base import Info
 from .preprocess import (To3D, Refit, Resample, TStat, Automask, Volreg, Merge,
                          ZCutUp, Calc, TShift, Warp, Detrend, Despike, Copy,
                          Fourier, Bandpass, Allineate, Maskave, SkullStrip,
-                         TCat, Fim, TCorrelate, BrickStat, ROIStats)
+                         TCat, Fim, TCorrelate, BrickStat, ROIStats, BlurInMask)
