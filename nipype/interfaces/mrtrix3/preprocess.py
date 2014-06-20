551b3ebdc06ab344ed49da81180c263688641688
bpinsard <basile.pinsard@imed.jussieu.fr>
2014-06-20 22:42:57 +0200

    wip

--- nipype/interfaces/mrtrix3/preprocess.py
+++ nipype/interfaces/mrtrix3/preprocess.py
@@ -5,15 +5,16 @@ class Dwi2ResponseInputSpec(MRtrixDwiCommandInputSpec):
     
     dwi = File(
         argstr="%s",
-        position=-1,
+        position=-2,
+        mandatory=True,
         desc="the input diffusion-weighted images")
 
     response = File(
         argstr="%s",
         position=-1,
-        name_source="dwi_in",
+        name_source="dwi",
+        name_template='%s_response',
         desc="the output rotational harmonic coefficients")
-    
 
     # DW Shell selection options
 
@@ -46,6 +47,7 @@ class Dwi2ResponseInputSpec(MRtrixDwiCommandInputSpec):
      is terminated.""")
 
     # Thresholds for single-fibre voxel selection
+
 
     volume_ratio = traits.Float(
         argstr = "-volume_ratio %f",
@@ -88,25 +90,33 @@ class Dwi2Response(MRtrixCommand):
     input_spec = Dwi2ResponseInputSpec
     output_spec = Dwi2ResponseOutputSpec
 
+    def _format_arg(self, name, trait_spec, value):
+        if name=='shells':
+            value = value.join(',')
+        return super(Dwi2Response,self)._format_arg(name, trait_spec,value)
+
+
 class Dwi2FodInputSpec(MRtrixDwiCommandInputSpec):
     dwi = File(
-        argstrs='%s',
+        argstr='%s',
         mandatory = True,
         position = -3,
         desc='the input diffusion-weighted image.')
 
     response = File(
-        position='-2',
+        argstr='%s',
+        position=-2,
         mandatory = True,
         desc=""" the diffusion-weighted signal response function for a
                      single fibre population, either as a comma-separated
                      vector of floating-point values, or a text file containing
                      the coefficients.""")
     sh_out_file = File(
-        argstrs='%s',
+        argstr='%s',
         mandatory = True,
-        position = -3,
+        position = -1,
         name_source = 'dwi',
+        name_template='%s_fod',
         desc = 'the output spherical harmonics coefficients image')
     
     
