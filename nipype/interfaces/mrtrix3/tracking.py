551ccc622d950bdb1debb8802440b8a0eda3d7d1
bpinsard <basile.pinsard@gmail.com>
2014-06-23 12:16:52 +0200

    tckgen wip

---
+++ nipype/interfaces/mrtrix3/tracking.py
@@ -0,0 +1,253 @@
+from nipype.interfaces.base import traits, File, InputMultiPath, isdefined
+from .base import MRtrixCommandInputSpec, MRtrixCommandOutputSpec, MRtrixCommand
+
+
+class TckgenInputSpec(MRtrixCommandInputSpec):
+    
+    """
+    perform streamlines tractography.
+    
+    """
+
+    source = File(
+        argstr = '%s',
+        position=-2,
+        mandatory = True,
+        exists = True,
+        desc="""the image containing the source data. 
+                     The type of data depends on the algorithm used:
+                     - FACT: the directions file (each triplet of volumes is
+                     the X,Y,Z direction of a fibre population).
+                     - iFOD1/2 & SD_Stream: the SH image resulting from CSD.
+                     - Nulldist & SeedTest: any image (will not be used).
+                     - TensorDet / TensorProb: the DWI image.""")
+    
+    tracks = File(
+        argstr = '%s',
+        position=-2,        
+        desc='the output file containing the tracks generated.')
+
+    algorithm = traits.Enum(
+        'FACT', 'iFOD1', 'iFOD2', 'Nulldist', 'SD_Stream', 'Seedtest', 'Tensor_Det', 'Tensor_Prob',
+        argstr = '-algorithm %s',
+        desc="""
+     specify the tractography algorithm to use. Valid choices are: FACT, iFOD1,
+     iFOD2, Nulldist, SD_Stream, Seedtest, Tensor_Det, Tensor_Prob (default:
+     iFOD2).""")
+
+    #Region Of Interest processing options
+
+    include = traits.Either(
+        File(),traits.Tuple([traits.Float]*4),
+        argstr = '-include %s',
+        desc="""
+     specify an inclusion region of interest, as either a binary mask image, or
+     as a sphere using 4 comma-separared values (x,y,z,radius). Streamlines
+     must traverse ALL inclusion regions to be accepted.""")
+
+    exclude = traits.Either(
+        File(),traits.Tuple([traits.Float]*4),
+        argstr = '-exclude %s',
+        desc="""
+     specify an exclusion region of interest, as either a binary mask image, or
+     as a sphere using 4 comma-separared values (x,y,z,radius). Streamlines
+     that enter ANY exclude region will be discarded.""")
+
+    mask = traits.Either(
+        File(),traits.Tuple([traits.Float]*4),
+        argstr = '-mask %s',
+        desc="""
+     specify a masking region of interest, as either a binary mask image, or as
+     a sphere using 4 comma-separared values (x,y,z,radius). If defined,
+     streamlines exiting the mask will be truncated.""")
+
+    #Streamlines tractography options
+
+    gradient = File(
+        argstr='-grad %s',
+        desc= """
+     specify the diffusion encoding scheme (may be required for Tensor_Det and
+     Tensor_Prob, ignored otherwise)""")
+
+    step = traits.Float(
+        argstr='-step %f',
+        desc="""
+     set the step size of the algorithm in mm (default is 0.1 x voxelsize; for
+     iFOD2: 0.5 x voxelsize).""")
+
+    angle = traits.Float(
+        argstr= '-angle %f',
+        desc="""
+     set the maximum angle between successive steps (default is 90deg x
+     stepsize / voxelsize).""")
+
+    number = traits.Int(
+        argstr='-number %d',
+        desc="""
+     set the desired number of tracks. The program will continue to generate
+     tracks until this number of tracks have been selected and written to the
+     output file.""")
+
+    maxnum = traits.Int(
+        argstr = '-maxnum %d',
+        desc = """
+     set the maximum number of tracks to generate. The program will not
+     generate more tracks than this number, even if the desired number of
+     tracks hasn't yet been reached (default is 100 x number).""")
+
+    maxlength = traits.Float(
+        argstr = '-maxlength %f',
+        desc = """
+    set the maximum length of any track in mm (default is 100 x voxelsize).""")
+
+    minlength = traits.Float(
+        argstr = '-minlength %f',
+        desc = """
+    set the minimum length of any track in mm (default is 5 x voxelsize).""")
+
+    cutoff = traits.Float(
+        argstr = '-cutoff %f',
+        desc="""set the FA or FOD amplitude cutoff for terminating tracks
+                (default is 0.1).""")
+
+    initcutoff = traits.Float(
+        argstr = '-initcutoff %f',
+        desc = """
+     set the minimum FA or FOD amplitude for initiating tracks (default is
+     twice the normal cutoff).""")
+
+    trials = traits.Int(
+        argstr = '-trials %d',
+        desc= """
+     set the maximum number of sampling trials at each point (only used for
+     probabilistic tracking).""")
+
+    unidirectional = traits.Bool(
+        argstr='-unidirectional',
+        desc="""
+     track from the seed point in one direction only (default is to track in
+     both directions).""")
+
+    initdirection = traits.Tuple(
+        (traits.Float(),)*3,
+        argstr = '-initdirection %f',
+        sep = ',',
+        desc = """
+     specify an initial direction for the tracking (this should be supplied as
+     a vector of 3 comma-separated values.""")
+
+    noprecomputed = traits.Bool(
+        argstr='-noprecomputed',
+        desc="""
+     do NOT pre-compute legendre polynomial values. Warning: this will slow
+     down the algorithm by a factor of approximately 4.""")
+
+    power = traits.Float(
+        argstr = '-power %f',
+        desc = 'raise the FOD to the power specified (default is 1/nsamples).')
+
+    samples = traits.Int(
+        argstr = '-samples %d',
+        desc="""
+     set the number of FOD samples to take per step for the 2nd order (iFOD2)
+     method (Default: 4).""")
+
+    rk4 = traits.Bool(
+        argstr = '-rk4',
+        desc="""
+     use 4th-order Runge-Kutta integration (slower, but eliminates curvature
+     overshoot in 1st-order deterministic methods)""")
+
+    stop = traits.Bool(
+        argstr = '-stop',
+        desc = 'stop propagating a streamline once it has traversed all include regions')
+    
+    downsample = traits.Float(
+        argstr = '-downsample %f',
+        desc='downsample the generated streamlines to reduce output file size')
+
+    #Anatomically-Constrained Tractography options
+
+    act = File(
+        argstr = '-act %s',
+        desc="""
+     use the Anatomically-Constrained Tractography framework during tracking;
+     provided image must be in the 5TT (five-tissue-type) format""")
+
+    backtrack = traits.Bool(
+        argstr='-backtrack',
+        desc="""
+     allow tracks to be truncated and re-tracked if a poor structural
+     termination is encountered""")
+
+    crop_at_gmwmi = traits.Bool(
+        argstr = '-crop_at_gmwmi',
+        desc = 'crop streamline endpoints more precisely as they cross the GM-WM interface')
+
+    #Tractography seeding options
+
+    seeding = traits.Tuple(
+        [traits.Float]*4,
+        argstr = '-seed_sphere %s',
+        sep=',',
+        desc='spherical seed as four comma-separated values (XYZ position and radius)')
+
+    seed_image = File(
+        argstr='-seed_image %s',
+        desc = """
+     seed streamlines entirely at random within a mask image (this is the same
+     behaviour as the streamline seeding in MRtrix 0.2)""")
+
+    seed_random_per_voxel = traits.Tuple(
+        File(),traits.Int(),
+        argstr = '-seed_random_per_voxel %s %d',
+        desc = """
+     seed a fixed number of streamlines per voxel in a mask image; random
+     placement of seeds in each voxel""")
+
+    seed_grid_per_voxel = traits.Tuple(
+        File(), traits.Int(),
+        argstr = '-seed_grid_per_voxel %s %d',
+        desc="""
+     seed a fixed number of streamlines per voxel in a mask image; place seeds
+     on a 3D mesh grid (grid_size argument is per axis; so a grid_size of 3
+     results in 27 seeds per voxel)""")
+
+    seed_rejection = File(
+        argstr = '-seed_rejection %s',
+        desc = """
+     seed from an image using rejection sampling (higher values = more probable
+     to seed from)""")
+
+    seed_gmwmi = File(
+        argstr = '-seed_gmwmi %s',
+        desc = """
+     seed from the grey matter - white matter interface (only valid if using
+     ACT framework)""")
+
+    seed_dynamic = File(
+        argstr = '-seed_dynamic %s',
+        desc = """
+     determine seed points dynamically using the SIFT model (must NOT provide
+     any other seeding mechanism)""")
+
+    max_seed_attempts = traits.Int(
+        argstr = '-max_seed_attempts %d',
+        desc = """
+     set the maximum number of times that the tracking algorithm should attempt
+     to find an appropriate tracking direction from a given seed point""")
+
+    output_seeds = File(
+        argstr = '-output_seeds %s',
+        desc = 'output the seed location of all successful streamlines to a file')
+    
+
+class TckgenOutputSpec(MRtrixCommandOutputSpec):
+
+class Tckgen(MRtrixCommand):
+    input_spec = TckgenInputSpec
+    output_spec = TckgenOutputSpec
+
+    
+
+
